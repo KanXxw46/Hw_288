aaa4327419958531032907beedcd69c4488d6324
Андрей Кан <1kan_andreikkkk@mail.ru>
2021-01-26 16:01:14 +0600

    Tamplate homeWork

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Hw288
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarMaking carMaking = new HeavyCar3();
+            carMaking = new LikerCoffee(carMaking);
+            Console.WriteLine(carMaking.Name + " " + carMaking.getCost());
+        }
+    }
+}
---
+++ Template.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hw288
+{
+    abstract class CarMaking
+    {
+        public void Make()
+        {
+            getBox();
+            addWheels();
+            addMotor();
+            addBody();
+        }
+       
+        public abstract void getBox();
+        public abstract void addWheels();
+        public abstract void addMotor();
+        public abstract void addBody();
+    }
+
+    class HeavyCar3 : CarMaking
+    {
+        
+
+        public override void getBox()
+        {
+            Console.WriteLine("Встовляем коробку передач.");
+        }
+
+        public override void addWheels()
+        {
+            Console.WriteLine("Закручиваем колесо.");
+        }
+
+        public override void addMotor()
+        {
+            Console.WriteLine("Вставляем мотор.");
+        }
+
+        public override void addBody()
+        {
+            Console.WriteLine("Стваи корбус поверз всего.");
+        }
+        
+    }
+    
+    }
+    class Template
+    {
+
+    }
+}
